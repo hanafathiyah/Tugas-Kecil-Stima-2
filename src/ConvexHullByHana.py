27cb3199bef864671e5d3a92dfd6272a7ac53b04
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-02-27 22:26:04 +0700

    progres malam ini

--- src/ConvexHullByHana.py
+++
@@ -1,9 +0,0 @@
-# Nama  : Hana Fathiyah
-# NIM   : 13520047
-# Kelas : K02
-
-import numpy as np
-
-def myConvexHull(bucket):
-  if len(bucket) <= 2:
-    return bucket
--- src/area.py
+++ src/area.py
@@ -10,3 +10,16 @@ def membuat_area(titik1, titik2, array_titik):
     # mencari kemiringan dan titik potong sumbu-y dengan y = mx + c
     # m = (y2 - y1)/(x2 - x1)
     m = (titik2[1] - titik1[1]) /(titik2[0] - titik1[0])
+    # c = - mx + y
+    c = -1 * m * titik1[0] + titik1[1]
+
+    for sumbu in array_titik:
+        # deteksi letak suatu titik
+        if (sumbu[1] > m * (sumbu[0]) + c): 
+            # titik di atas garis
+            area_atas.append(sumbu)
+        elif (sumbu[1] < m * (sumbu[0]) + c): 
+            # titik di bawah garis
+            area_bawah.append(sumbu)
+    
+    return area_atas, area_bawah
