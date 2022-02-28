4f385be3d0e11e160dcb469022cdc7ce759895fb
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-02-28 13:41:29 +0700

    merapikan

--- src/debug.py
+++
@@ -1,7 +0,0 @@
-from convex_hull_by_hana import myConvexHull
-from convex_hull_by_hana import fungsi_convex_hull_atas
-from convex_hull_by_hana import membuat_area
-
-import numpy as np
-tes = [np.array([1,2]), np.array([4,3]), np.array([4,6])]
-print(fungsi_convex_hull_atas(np.array([0,0]),np.array([5,5]), tes))
--- src/titik_menjadi_indeks.py
+++
@@ -1,5 +0,0 @@
-def titik_menjadi_indeks(titik_x, titik_y, array_of_titik):
-    for i in range(array_of_titik.shape[0]):
-        for j in range(array_of_titik.shape[1]):
-            if(titik_x == array_of_titik[i] and titik_y == array_of_titik[j]):
-                return i,j;
