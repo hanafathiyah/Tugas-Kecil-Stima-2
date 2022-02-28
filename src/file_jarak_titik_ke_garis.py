a6bf604ecfc98206643b9718b2fb4b66a74fe89e
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-02-28 16:13:03 +0700

    last commit

--- src/file_jarak_titik_ke_garis.py
+++ src/file_jarak_titik_ke_garis.py
@@ -2,12 +2,6 @@ import numpy as np
 
 def jarak_titik_ke_garis(titik1, titik2, titik3):
   
-    # dimisalkan suatu garis l terbentuk dari titik1 dan titik2
-    # fungsi akan me-return jarak titik3 ke garis l tersebut
-
-    # Ax + By + C = 0
-    # (y - y1) = m(x - x1)
-    # (y - y1)/(y2 - y1) = (x - x1)/(x2 - x1)
     titik_1_as_numpy = np.array(titik1)
     titik_2_as_numpy = np.array(titik2)
     titik_3_as_numpy = np.array(titik3)
@@ -15,6 +9,5 @@ def jarak_titik_ke_garis(titik1, titik2, titik3):
     vektor_a = titik_3_as_numpy - titik_1_as_numpy
     vektor_b = titik_2_as_numpy - titik_1_as_numpy
     vektor_c = np.sum(vektor_a * vektor_b) / np.sum(vektor_b * vektor_b) * vektor_b - vektor_a
-    # rumus | Ax + By + C | / akar(A^2 + B^2)
 
     return np.sum(vektor_c * vektor_c)
